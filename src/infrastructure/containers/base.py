2d3eeb7984402d37bbfe1a12f4e64bfa8a501cc1
Max <maxim@akimkin>
2025-01-18 13:46:06 +0100

    🍆 feat: dispatcher & calculator compute command added to the di container

--- src/infrastructure/containers/base.py
+++ src/infrastructure/containers/base.py
@@ -6,9 +6,16 @@ from typing import AsyncIterable
 
 from dishka import Scope, Provider, provide
 
+from application.calculator.commands import (
+    CalculatorComputeCommandHandler,
+    CalculatorComputeCommand,
+    #
+)
+
 # fmt: off
 from ..calculators.rpn import CalculatorRPN
 from ..calculators.base import CalculatorBase
+from ..dispatchers.dispatcher import Dispatcher
 from ..authenticator.base import AuthenticatorBase
 from ..authenticator.argon import ArgonAuthenticator
 from ..connection_manager.common.base import ConnectionManagerBase
@@ -41,6 +48,25 @@ class ContainerBase(Provider):
             access_token_expiration_seconds=config.AUTH_ACCESS_TOKEN_EXPIRATION_SECONDS,
             refresh_token_expiration_seconds=config.AUTH_REFRESH_TOKEN_EXPIRATION_SECONDS,
         )
+
+    @provide(scope=Scope.APP)
+    def get_dispatcher(
+        self,
+        calculator: CalculatorBase,
+    ) -> Dispatcher:
+        dispatcher = Dispatcher()
+
+        # COMMANDS
+        calculator_compute_command_handler = CalculatorComputeCommandHandler(
+            calculator=calculator,
+        )
+
+        dispatcher.register_command(
+            CalculatorComputeCommand,
+            calculator_compute_command_handler,
+        )
+
+        return dispatcher
 
 
 # endregion-------------------------------------------------------------------------
