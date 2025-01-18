703c0b792aa252da82b0e6b33b3401d55a8de578
Max <maxim@akimkin>
2025-01-18 15:26:41 +0100

    🍓🍒 refactor+fix: legacy inside di container

--- src/infrastructure/containers/app.py
+++ src/infrastructure/containers/app.py
@@ -2,13 +2,11 @@
 # region CONTAINER APP
 # ----------------------------------------------------------------------------------
 from .base import ContainerBase
-from .cache.redis import RedisCacheContainer
 from .repositories.sql import SQLRepositoriesContainer
 
 
 class ContainerApp(
     ContainerBase,
-    RedisCacheContainer,
     SQLRepositoriesContainer,
 ): ...
 
--- src/infrastructure/containers/mock.py
+++ src/infrastructure/containers/mock.py
@@ -2,13 +2,11 @@
 # region CONTAINER MOCK
 # ----------------------------------------------------------------------------------
 from .repositories.memory import MemoryRepositoriesContainer
-from .cache.memory import MemoryCacheContainer
 from .base import ContainerBase
 
 
 class ContainerMock(
     ContainerBase,
-    MemoryCacheContainer,
     MemoryRepositoriesContainer,
 ): ...
 
