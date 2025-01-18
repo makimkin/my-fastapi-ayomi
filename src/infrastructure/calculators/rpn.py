3704443d0f72c6c8d0a73cbdd3d0f683d20ae529
Max <maxim@akimkin>
2025-01-18 13:10:47 +0100

    🍆 feat: implementing rpn calculator

--- src/infrastructure/calculators/rpn.py
+++ src/infrastructure/calculators/rpn.py
@@ -4,6 +4,7 @@
 import logging
 
 from dataclasses import dataclass
+from decimal import Decimal
 
 from infrastructure.calculators.base import CalculatorBase
 
@@ -12,8 +13,36 @@ logger = logging.getLogger("app")
 
 @dataclass
 class CalculatorRPN(CalculatorBase):
-    async def compute(self, expression: str) -> float:
-        return len(expression)
+    OPERANDS = {
+        "-": lambda e1, e2: e1 - e2,
+        "*": lambda e1, e2: e1 * e2,
+        "/": lambda e1, e2: e1 / e2,
+        "+": lambda e1, e2: e1 + e2,
+    }
+
+    async def compute(self, expression: str) -> Decimal:
+        items = expression.strip().split(" ")
+
+        stack = []
+
+        for i in items:
+            if i.isdigit():
+                stack.append(Decimal(i))
+                continue
+
+            if i in self.OPERANDS:
+                if len(stack) < 2:
+                    raise ValueError("Invalid expression")
+
+                x2 = stack.pop()
+                x1 = stack.pop()
+
+                stack.append(self.OPERANDS[i](x1, x2))
+                continue
+
+            raise ValueError("Invalid expression")
+
+        return stack.pop()
 
     async def check_health(self) -> bool:
         return True
