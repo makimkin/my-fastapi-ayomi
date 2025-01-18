318cdf4b577d71115fc6194b6e3f96ce1b6039b8
Max <maxim@akimkin>
2025-01-18 14:44:06 +0100

    🍆 feat: added test for checking that there is no floating problem

--- src/tests/e2e/test_calculator_compute.py
+++ src/tests/e2e/test_calculator_compute.py
@@ -4,7 +4,6 @@
 import pytest
 
 from fastapi import status
-from faker import Faker
 
 from interface.api.calculator.base import CALCULATOR_ACTIONS, CALCULATOR_PREFIX
 from interface.api.calculator.schemas import (
@@ -16,12 +15,9 @@ from .conftest import ContextTest
 
 
 @pytest.mark.asyncio
-async def test_calculator_compute_success(
-    context: ContextTest,
-    faker: Faker,
-) -> None:
+async def test_calculator_compute_success(context: ContextTest):
     """-----------------------------------------------------------------------------
-    Test health check success.
+    Test: 'POST' request to compute a simple expression.
     -----------------------------------------------------------------------------"""
     # fmt: off
     expression = "3 4 +"
@@ -37,6 +33,28 @@ async def test_calculator_compute_success(
     calculator_compute_data = CalculatorComputeResponse(**calculator_compute_json)
     assert calculator_compute_data.expression == expression, text
     assert calculator_compute_data.result == 7, text
+    # fmt: on
+
+
+@pytest.mark.asyncio
+async def test_calculator_compute_success_floating_problem(context: ContextTest):
+    """-----------------------------------------------------------------------------
+    Test: 'POST' request to compute a simple expression with floating point numbers.
+    -----------------------------------------------------------------------------"""
+    # fmt: off
+    expression = "0.1 0.2 +"
+
+    calculator_compute_request = CalculatorComputeRequest(expression=expression)
+    calculator_compute_response = await context.post(f"/v1{CALCULATOR_PREFIX}{CALCULATOR_ACTIONS.COMPUTE}", json=calculator_compute_request.model_dump())
+    assert calculator_compute_response.status_code == status.HTTP_200_OK, calculator_compute_response.text
+
+    calculator_compute_json, text = calculator_compute_response.json(), calculator_compute_response.text
+    assert "expression" in calculator_compute_json, text
+    assert "result" in calculator_compute_json, text
+
+    calculator_compute_data = CalculatorComputeResponse(**calculator_compute_json)
+    assert calculator_compute_data.expression == expression, text
+    assert calculator_compute_data.result == 0.3, text
     # fmt: on
 
 
