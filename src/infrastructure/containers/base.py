52bab1ba9671e00cc5ecbb041c582dedad7ed002
Max <maxim@akimkin>
2025-01-18 15:27:07 +0100

    🍓 refactor: removed connection manager (not used)

--- src/infrastructure/containers/base.py
+++ src/infrastructure/containers/base.py
@@ -2,7 +2,6 @@
 # region CONTAINER BASE
 # ----------------------------------------------------------------------------------
 from settings.config import Config
-from typing import AsyncIterable
 
 from dishka import Scope, Provider, provide
 
@@ -16,8 +15,6 @@ from application.calculator.commands import (
 from ..calculators.rpn import CalculatorRPN
 from ..calculators.base import CalculatorBase
 from ..dispatchers.dispatcher import Dispatcher
-from ..connection_manager.common.base import ConnectionManagerBase
-from ..connection_manager.fastapi.concurrent import FastAPIConnectionManagerConcurrent
 # fmt: on
 
 
@@ -25,14 +22,6 @@ class ContainerBase(Provider):
     @provide(scope=Scope.APP)
     def get_config(self) -> Config:
         return Config()
-
-    @provide(scope=Scope.APP)
-    async def get_connection_manager(self) -> AsyncIterable[ConnectionManagerBase]:
-        connection_manager = FastAPIConnectionManagerConcurrent()
-
-        yield connection_manager
-
-        await connection_manager.close()
 
     @provide(scope=Scope.APP)
     def get_calculator(self) -> CalculatorBase:
